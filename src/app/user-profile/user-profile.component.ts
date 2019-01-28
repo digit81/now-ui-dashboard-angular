93f07fb12edccecba1698c8b1c40bb8089e9476d
digit81 <dargrab@protonmail.com>
2019-01-28 20:51:32 +0100

    Data binding for profile.

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -6,6 +6,15 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
+  username = 'michael23';
+  email = 'hello@creative-tim.com';
+  firstName = 'Mike';
+  lastName = 'Andrew';
+  address = 'Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09';
+  city = 'Bucharest';
+  country = 'Romania';
+  zipCode = '050107';
+  description = 'Lamborghini Mercy, Your chick she so thirsty, I\'m in that two seat Lambo.';
 
   constructor() {
   }
